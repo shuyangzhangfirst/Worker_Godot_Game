0c1f41a33beafec101a0494456513e349d5b4e7a
ChangLuoD <479047400@qq.com>
2026-05-19 15:11:14 +0800

    场景过与地图基类初始模型

---
+++ script/component/map/map.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name BaseMap
+
+@onready var env_color: CanvasModulate = $EnvColor	## 地图环境色调
+
+@export var map_name: String = ""	## 地图名称
+@export var map_music: AudioStream	## 地图音乐
+
+## 地图事件数据
+@export var map_data: Dictionary = {}
+
+var map_path: String = scene_file_path
+
+func _ready() -> void:
+	_init_map()
+
+func _init_map() -> void:
+	GameSystem.audio.play_music(map_music)
