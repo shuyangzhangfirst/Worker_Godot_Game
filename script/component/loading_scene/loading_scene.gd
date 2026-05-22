ff369cbfbe4efdac8db15fa79529a3f02b7deecf
ChangLuoD <479047400@qq.com>
2026-05-22 23:25:44 +0800

    加载进度优化

--- script/component/loading_scene/loading_scene.gd
+++ script/component/loading_scene/loading_scene.gd
@@ -5,19 +5,35 @@ signal loading_scene_ready	## 加载场景就绪
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var loading_container: VBoxContainer = %LoadingContainer
-
+@onready var timer: Timer = %Timer
 @onready var loading_progress_bar: ProgressBar = %LoadingProgressBar
 @onready var loading_number: Label = %LoadingNumber
-@onready var label_animation: Label = $LoadingContainer/HBoxContainer/加载文字动画/LabelAnimation
+@onready var label_animation: Label = %LabelAnimation
+
+var point_index: int = 0
+var point_string: Array[String] = [
+	".     ",
+	"..    ",
+	"...   ",
+	"....  ",
+	"..... ",
+	"......"
+]
 
 func _ready() -> void:
 	await animation_player.animation_finished
 	_show_loading_progress()
+	await get_tree().create_timer(0.2).timeout
 	loading_scene_ready.emit()
+
+func _physics_process(_delta: float) -> void:
+	if timer.is_stopped():
+		_update_point_ani()
+		timer.start()
 
 ## 改变进度
 func _on_progress_changed(new_value: float) -> void:
-	var value: float = new_value * 100
+	var value: int = int(new_value * 100)
 	loading_number.text = str(value)
 	loading_progress_bar.value = value
 
@@ -35,3 +51,7 @@ func _show_loading_progress() -> void:
 	loading_number.text = "0"
 	loading_progress_bar.value = 0
 	loading_container.visible = true
+	
+func _update_point_ani() -> void:
+	label_animation.text = point_string[point_index]
+	point_index = wrapi(point_index + 1, 0, point_string.size())
