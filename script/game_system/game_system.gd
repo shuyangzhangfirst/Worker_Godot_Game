a43b75c8ba1a7ebcc37be282d025de83cfdc74a5
ChangLuoD <479047400@qq.com>
2026-05-22 16:22:29 +0800

    测试场景与加载界面,交互器原型

--- script/game_system/game_system.gd
+++ script/game_system/game_system.gd
@@ -4,5 +4,6 @@ extends Node
 @onready var data: DataManager = %Data
 @onready var audio: AudioManager = %Audio
 @onready var settings: SettingsManager = %Settings
+@onready var scene_load: SceneLoad = %SceneLoad
 
 var main_scene: Node2D	## 游戏主场景
