74a5977affa68a292f187e55f08f78a490d4358d
ChangLuoD <479047400@qq.com>
2026-06-09 03:07:45 +0800

    更新车辆纹理

---
+++ script/class/player_attribute/player_attribute.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name PlayerAttribute
+
+@export var base_speed: float = 300.0
+
+@export var health: int = 100
+@export var max_health: int = 100
+
+@export var stamina: float = 100.0
+@export var max_stamina: int = 100
--- script/game_system/data/data_manager.gd
+++ script/game_system/data/data_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 class_name DataManager
 
+
 var current_player:Player## 当前玩家节点
 	
