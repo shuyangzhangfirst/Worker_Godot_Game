08ae3222aa47d2f30149b408a2f06ddc28907e6c
shuyangzhang <25999zhang@gmail.com>
2026-07-07 14:00:07 +0200

    change value

--- script/game_system/EventBus/eventbus.gd
+++ script/game_system/EventBus/eventbus.gd
@@ -2,7 +2,7 @@ extends Node
 
 signal scene_load_progress_changes(progress: Array[float])	## 场景加载进度改变
 signal scene_load_finished		## 场景加载完成
-
+signal player_stat_changed(stat_name:String)
 #region 交互器列表相关
 signal append_on_interactors(interactor: Interactor)	## 在交互器列表增加交互器
 signal erase_on_interactors(interactor: Interactor)		## 在交互器列表增移除交互器
